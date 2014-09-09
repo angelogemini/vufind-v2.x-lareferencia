ff4ac3c6f228cabccd2e64d7033536cb9b5dc101
lmatas <lmatas@gmail.com>
2014-09-09 16:37:37 -0300

    Agregado del controlador estadísticas por clicks, bajo el módulo LaReferencia

--- module/LaReferencia/config/module.config.php
+++ module/LaReferencia/config/module.config.php
@@ -2,6 +2,29 @@
 namespace LaReferencia\Module\Configuration;
 
 $config = array(
+    'controllers' => array(
+        'invokables' => array(
+            'clickstats' => 'LaReferencia\Controller\ClickStatsController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'clickstats' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/ClickStats',
+                    'defaults' => array(
+                        'controller' => 'ClickStats',
+                        'action'     => 'Home',
+                    )
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                   
+                ),
+            ),
+        ),
+    ),
 );
 
 return $config;
