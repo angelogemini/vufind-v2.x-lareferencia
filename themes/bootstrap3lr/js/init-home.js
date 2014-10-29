7a130782e6be2223113a7ea28af29d5a0dc338be
lmatas <lmatas@gmail.com>
2014-10-29 08:32:39 -0400

    Implementación de sliders de socios y nodos cosechados

--- themes/bootstrap3lr/js/init-home.js
+++ themes/bootstrap3lr/js/init-home.js
@@ -1,9 +1,24 @@
 //toggle partner's images
 $(document).ready(function() {
 	
-$(".partner a").hover(function(e) {	
+$('#partners').flexslider({
+    animation: "slide",
+    easing: "swing",
+    animationLoop: true,
+    itemWidth: 1,
+    itemMargin:30,
+    minItems: 2,
+    maxItems: 8,
+    controlNav: false,
+    directionNav: false,
+    move: 2
+});
+		
+$(".pick-nodes a").hover(function(e) {	
 	e.preventDefault();
     $(this).find('img').toggle();
 });
 
 });
+
+
